45bf733fa2864ee7180b4a956d1e951385c9ad46
nathanvda <nathan@dixis.com>
2010-10-04 22:52:24 +0200

    prepare code for select to work WIP

--- lib/on_the_spot/on_the_spot_helpers.rb
+++ lib/on_the_spot/on_the_spot_helpers.rb
@@ -4,11 +4,15 @@ module OnTheSpot
     EDIT_TYPE_TEXTAREA='textarea'
     EDIT_TYPE_SELECT='select'
 
+    class OnTheSpotMissingParameters < StandardError; end
+
     # Possible options:
     #   ok_text     : the ok-button text
     #   cancel_text : the cancel-button text
     #   tooltip     : the tooltip to show
     #   type        : {'textarea' || 'select' }
+    #   loadurl     : (for select) an url that will return the json for the select
+    #   data        : (for select) an array of options in the form [id, value]
     def on_the_spot_edit(object, field, options={})
       #!!! to do: translate options to data-fields
       # Possible fields:
@@ -23,18 +27,48 @@ module OnTheSpot
 
       update_url = url_for(:action => 'update_attribute_on_the_spot')
 
+      field_value =  object.send(field.to_sym).to_s
+
       html_options = { :id => "#{object.class.name.underscore}__#{field}__#{object.id}",
                        :class => 'on_the_spot_editing',
                        :'data-url' => update_url}
 
-      html_options[:'data-edittype'] = options[:type] unless options[:type].nil?
+      editable_type = options[:type].to_sym
+      html_options[:'data-edittype'] = editable_type.to_s unless editable_type.nil?
+      if editable_type == :select
+        # we need either a data or loadurl
+        unless options[:loadurl].nil?
+          html_options[:'data-loadurl'] = options[:loadurl]
+        else
+          # we should find a hash
+          select_data = options[:data]
+          raise OnTheSpotMissingParameters.new("Using type select needs either data or loadurl to function!") if select_data.nil?
+          options[:'data-select'] = convert_array_to_json(select_data, field_value)
+        end
+      end
       html_options[:'data-ok']       = options[:ok_text] unless options[:ok_text].nil?
       html_options[:'data-cancel']   = options[:cancel_text] unless options[:cancel_text].nil?
       html_options[:'data-tooltip']  = options[:tooltip] unless options[:tooltip].nil?
 
       content_tag("span", html_options) do
-        object.send(field.to_sym).to_s
+        if editable_type == :select
+          lookup_display_value(select_data, field_value)
+        else
+          field_value
+        end
       end
+    end
+
+    def lookup_display_value(id_value_array, id_str)
+      id_value_array.select{ |idv| idv[0].to_s == id_str }[1]
+    end
+
+    def convert_array_to_json(id_value_array, selected_id)
+      conv_arr = id_value_array.map{|idv| "'#{idv[0]}':'#{idv[1]}'" }
+      result = "{ #{conv_arr.join', '} "
+      result += ", 'selected': '#{ selected_id.to_s}'" unless selected_id.nil?
+      result += "}"
+      result
     end
 
   end
