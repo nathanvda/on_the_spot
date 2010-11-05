6af8574f4f5f7e00d78634f26049ef9eeff5980a
nathanvda <nathan@dixis.com>
2010-11-05 22:48:43 +0100

    Use update_attributes instead of update_attribute, and handle validation-errors (as a consequence) correctly.

--- lib/on_the_spot/controller_extension.rb
+++ lib/on_the_spot/controller_extension.rb
@@ -13,12 +13,15 @@ module OnTheSpot
           klass, field, id = params[:id].split('__')
           select_data = params[:select_array]
           object = klass.camelize.constantize.find(id)
-          object.update_attribute(field, params[:value])
-          if select_data.nil?
-            render :text => CGI::escapeHTML(object.send(field).to_s)
+          if object.update_attributes(field => params[:value])
+            if select_data.nil?
+              render :text => CGI::escapeHTML(object.send(field).to_s)
+            else
+              parsed_data = JSON.parse(select_data.gsub("'", '"'))
+              render :text => parsed_data[object.send(field).to_s]
+            end
           else
-            parsed_data = JSON.parse(select_data.gsub("'", '"'))
-            render :text => parsed_data[object.send(field).to_s]
+            render :text => object.errors.full_messages.join("\n"), :status => 422
           end
         end
       end
