fb7cdf2e1de3e1076f23b6960d57a8a64843e36a
Peter Savichev (proton) <psavichev@gmail.com>
2011-10-10 15:18:33 +0400

    fixed checkbox.js

--- app/assets/javascripts/jquery.jeditable.checkbox.js
+++ app/assets/javascripts/jquery.jeditable.checkbox.js
@@ -1,6 +1,7 @@
 /**********************************************************************
  *	Custom input types for the jquery.jeditable plugin
- * By Richard Davies <Richard__at__richarddavies.us>
+ * By Richard Davies <Richard__at__richarddavies.us>, 2009
+ * By Peter Savichev (proton) <psavichev@gmail.com>, 2011
  *********************************************************************/
 
 // Create a custom input type for checkboxes
@@ -9,18 +10,17 @@ $.editable.addInputType("checkbox", {
 		var input = $('<input type="checkbox">');
 		$(this).append(input);
 
-		// Update <input>'s value when clicked
-		$(input).click(function() {
-			var value = $(input).attr("checked") ? 'Yes' : 'No';
+		$(input).change(function() {
+			var value = $(input).attr("checked") ? 1 : 0;
 			$(input).val(value);
 		});
 		return(input);
 	},
 	content : function(string, settings, original) {
-		var checked = string == "Yes" ? 1 : 0;
+		var checked = (string == "true") ? 1 : 0;
 		var input = $(':input:first', this);
-		$(input).attr("checked", checked);
-		var value = $(input).attr("checked") ? 'Yes' : 'No';
-		$(input).val(value);
+		if(checked) $(input).attr("checked", "checked");
+		else $(input).removeAttr("checked");
+		$(input).val(checked);
 	}
 });
