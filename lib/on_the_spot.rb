50482f4528dcbd1a37292fca3486402fba6e12ee
Chris Barretto <chrisabarretto@gmail.com>
2013-07-16 15:53:27 -0700

    Make sure javascript_expansions config option exists

--- lib/on_the_spot.rb
+++ lib/on_the_spot.rb
@@ -5,7 +5,9 @@ module OnTheSpot
   class Engine < ::Rails::Engine
 
     config.before_initialize do
-      config.action_view.javascript_expansions[:on_the_spot] = %w(jquery.jeditable.mini.js on_the_spot_code)
+      if config.action_view.javascript_expansions
+        config.action_view.javascript_expansions[:on_the_spot] = %w(jquery.jeditable.mini.js on_the_spot_code)
+      end
     end
 
     # configure our plugin on boot. other extension points such
